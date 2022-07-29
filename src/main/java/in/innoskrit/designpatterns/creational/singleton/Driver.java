dbb02df214d33c79339ecebee1dcd17e0d0eb862
prerna1124 <prernakaruna24@gmail.com>
2022-07-29 21:42:43 +0530

    Reflection Problem Added

--- src/main/java/in/innoskrit/designpatterns/creational/singleton/Driver.java
+++ src/main/java/in/innoskrit/designpatterns/creational/singleton/Driver.java
@@ -1,6 +1,8 @@
 package in.innoskrit.designpatterns.creational.singleton;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 
 public class Driver {
@@ -45,7 +47,33 @@ public class Driver {
 
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    /**
+     * In this method, we are fetching all the constructors that are defined in the class, and we are making them accessible outside the class.
+     * And then we are creating an instance and comparing the hash codes for both the instances. And as a result of which we are getting two
+     * different instances created. Hence, it is breaking the rules of Singleton Class.
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public static void reflectionProblem() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        ThreadSafeSingletonClass instance1 = ThreadSafeSingletonClass.getInstance();
+        ThreadSafeSingletonClass instance2 = null;
+
+        Constructor[] constructors = ThreadSafeSingletonClass.class.getDeclaredConstructors();
+        for (Constructor constructor: constructors) {
+            constructor.setAccessible(true);
+            try {
+                instance2 = (ThreadSafeSingletonClass) constructor.newInstance();
+                break;
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException exception) {
+                exception.printStackTrace();
+            }
+        }
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
         // Testing SingletonClass
         Driver.testSingletonClass();
@@ -61,6 +89,9 @@ public class Driver {
 
         // Testing SingletonClassWithSerialization
         Driver.testSingletonClassWithSerialization();
+
+        // Testing Reflection Problem
+        Driver.reflectionProblem();
 
     }
 }
