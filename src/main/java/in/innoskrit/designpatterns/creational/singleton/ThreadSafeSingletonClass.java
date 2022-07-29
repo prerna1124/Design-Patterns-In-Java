3d21a1be8a2d0eec1d7fd5cef6a05f62832cfc4a
prerna1124 <prernakaruna24@gmail.com>
2022-07-29 20:39:23 +0530

    Singleton Class With Thread Safe Using Synchronized Block

--- src/main/java/in/innoskrit/designpatterns/creational/singleton/ThreadSafeSingletonClass.java
+++ src/main/java/in/innoskrit/designpatterns/creational/singleton/ThreadSafeSingletonClass.java
@@ -2,15 +2,34 @@ package in.innoskrit.designpatterns.creational.singleton;
 
 public class ThreadSafeSingletonClass {
 
-    private static ThreadSafeSingletonClass singletonClassInstance;
+    private static volatile ThreadSafeSingletonClass singletonClassInstance;
 
     private ThreadSafeSingletonClass() {
 
     }
 
+    /**
+     * This is one way to make this method thread safe with the use of synchronized keyword.
+     * @return singletonClassInstance   the singleton class instance
+     */
     public static synchronized ThreadSafeSingletonClass getInstance() {
         if(singletonClassInstance == null) {
             singletonClassInstance = new ThreadSafeSingletonClass();
+        }
+        return singletonClassInstance;
+    }
+
+    /**
+     * This is another way to make this method thread safe with the use of synchronized block.
+     * @return singletonClassInstance   the singleton class instance
+     */
+    public static ThreadSafeSingletonClass getInstance2() {
+        if(singletonClassInstance == null) {
+            synchronized (ThreadSafeSingletonClass.class) {
+                if (singletonClassInstance == null) {
+                    singletonClassInstance = new ThreadSafeSingletonClass();
+                }
+            }
         }
         return singletonClassInstance;
     }
